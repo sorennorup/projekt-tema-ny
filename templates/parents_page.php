d8ced68fdd5e714f08e0bb1987f4b6f5f22b4de7
Søren Nørup <soren.norup@uudanmark.dk>
2020-09-09 16:02:09 +0200

    adds logos for the parentspage footer

--- templates/parents_page.php
+++ templates/parents_page.php
@@ -9,13 +9,14 @@
 ?>
 
 <div class = "container-fluid">
-    <blockquote class = "blockquote wp-block-quote header-quote">    
-        <p> Fortæl mig noget, og jeg vil glemme det.
+    <div class = "quote-wrapper">
+    <blockquote class = "center blockquote wp-block-quote header-quote background">    
+        <i> Fortæl mig noget, og jeg vil glemme det.
             Vis mig en ting, og jeg husker det måske. 
             Giv mig en oplevelse, og jeg glemmer det aldrig </br>
-            - Søren Kierkegaard </p>
+            - Søren Kierkegaard </i>
     </blockquote>
-    
+    </div>
     <div class = "row nopadding">
         <div class=" first-col col-lg-4">
             <h2 class = "text-uppercase">
@@ -66,15 +67,32 @@
             </div>
         <br/>
         
-   
+        
         </div>
-     
-        
-        
-        
-        
+        <div class = "sponser_partner_container">
+           <div class = "container">
+         <div class = "row">
+          <div class = "col-sm-4"></div>
+           <div class = "col-sm-2"><img class = "img-responsive" src = "/wp-content/themes/projekt-tema-ny/partnerlogoer/nordea-fonden.png" width = "150" /> </div>
+            <div class = "col-sm-2"><img class = "img-responsive" src = "/wp-content/themes/projekt-tema-ny/partnerlogoer/UU-Danmark-logo.png" width = "150" /></div>
+      
+            <div class = "col-sm-4"></div>
+
+          </div> 
+         </div><br/>
+         
+         <div class = "container">
+         <div class = "row">
+          <div class = "col-sm-1"></div>
+           <div class = "bottom-margin col-sm-2 mb-5"><img class = "img-responsive" src = "/wp-content/themes/projekt-tema-ny/partnerlogoer/Dansk-Byggeri-logo_net.jpg" width = "150" /> </div>
+            <div class = " bottom-margin col-sm-2 mb-5"><img class = "img-responsive" src = "/wp-content/themes/projekt-tema-ny/partnerlogoer/DE_logo_2016.jpg" width = "150"  /> </div>
+             <div class = " bottom-margin left-margin col-sm-2 mb-5"><img class = "img-responsive" src = "/wp-content/themes/projekt-tema-ny/partnerlogoer/lf-logo-positiv.png" width = "150"  /> </div>
+              <div class = " bottom-margin col-sm-2 mb-5"><img class = "img-responsive" src = "/wp-content/themes/projekt-tema-ny/partnerlogoer/logo-danske-maritime-Dansk.jpg" width = "150"  /> </div>
+            <div class = " bottom-margin col-sm-2 mb-5"><img class = "img-responsive" src = "/wp-content/themes/projekt-tema-ny/partnerlogoer/Plastindustrien-logo-for-web-1.png" width = "150"  /> </div>
+            <div class = " col-sm-1 "></div>
+
+          </div> 
+         </div>
+</div>
     </body>
-    
-
-
-<?php get_footer(); ?>
+    </html>
