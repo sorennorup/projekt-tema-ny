114e875c44c005cfa29abb991743e29e966782ad
Søren Nørup <soren.norup@uudanmark.dk>
2020-09-22 10:44:42 +0200

    fixes error with wrong includefile in header

--- header.php
+++ header.php
@@ -16,7 +16,6 @@
 		<?php include_once('classes/BreadCrumbs.php'); ?>
 		<?php include_once('elements/CtaButton.php'); ?>
 		<?php include_once('classes/Menu.php'); ?>
-		<?php require_once('classes/CustomField.php');?>
 
 
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
