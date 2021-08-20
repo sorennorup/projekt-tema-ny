071084522838e279225fe1887e312f64b79f2060
Søren Nørup <soren.norup@uudanmark.dk>
2021-08-20 13:59:44 +0200

    test

---
+++ config_mapping.php
@@ -0,0 +1,5 @@
+<?php $username = 'admin@uudanmark.dk';
+$password = 'uuuc1508';
+$client_secret = "begivenheder";
+$client_token= "J4mtK43eJchKWbPsLggiLYrDBL0FCOjYF3CW6dq158yBLlbLB93HXgTIGw3FnBAe";
+?>
